3a78deeace35cdd968a3e9b384926c0d32c6471f
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-04-24 16:32:33 +0200

    custom graphics for dart-radar and technology

--- data.lua
+++ data.lua
@@ -83,16 +83,12 @@ dart_fcc_recipe.ingredients = {
 
 --- create the D.A.R.T-fcc entity
 local dart_fcc_entity = data_util.copy_prototype(data.raw['constant-combinator']['constant-combinator'], "dart-fcc")
-
-Log.logBlock(dart_fcc_entity, function(m)log(m)end, Log.FINE)
-
 dart_fcc_entity.icon = "__dart__/graphics/icons/fcc.png"
 dart_fcc_entity.icon_size = 64
 dart_fcc_entity.icon_mipmaps = 4
 dart_fcc_entity.next_upgrade = nil
 dart_fcc_entity.fast_replaceable_group = nil
 dart_fcc_entity.surface_conditions = surface_conditions()
-
 local image = "__dart__/graphics/entity/fcc.png"
 local dart_fcc_entity_update = {
     sprites = {
@@ -111,18 +107,25 @@ dart_fcc_entity = meld(dart_fcc_entity, dart_fcc_entity_update)
 
 Log.logBlock(dart_fcc_entity, function(m)log(m)end, Log.FINER)
 
-
 --- create the D.A.R.T-radar entity
 local dart_radar_entity = data_util.copy_prototype(data.raw["radar"]["radar"], "dart-radar")
-Log.logBlock(dart_radar_entity, function(m)log(m)end, Log.FINER)
+Log.logBlock(dart_radar_entity, function(m)log(m)end, Log.FINE)
 
-dart_radar_entity.icon = "__base__/graphics/icons/radar.png" -- TODO
+dart_radar_entity.icon = "__dart__/graphics/icons/radar.png"
 dart_radar_entity.icon_size = 64
 dart_radar_entity.icon_mipmaps = 4
 dart_radar_entity.next_upgrade = nil
-dart_radar_entity.rotation_speed = 0.02
+-- twice as fast and clockwise rotation - use of the neg. value is an undocumented feature, but works fine ;-)
+dart_radar_entity.rotation_speed = -0.02
 dart_radar_entity.energy_usage = "100kW"
 dart_radar_entity.fast_replaceable_group = nil
+
+local dart_radar_entity_update = {
+    pictures = {
+        layers = {{ filename  =  "__dart__/graphics/entity/radar.png"}}
+    }
+}
+dart_radar_entity = meld(dart_radar_entity, dart_radar_entity_update)
 
 rescale_entity(dart_radar_entity, 1 / 3)
 
@@ -147,7 +150,7 @@ Log.logBlock(dart_radar_entity, function(m)log(m)end, Log.FINER)
 --- create the D.A.R.T-radar item
 local dart_radar_item = data_util.copy_prototype(data.raw["item"]["radar"], "dart-radar")
 local order = dart_radar_item.order or "dart"
-dart_radar_item.icon = "__base__/graphics/icons/radar.png" -- TODO
+dart_radar_item.icon = "__dart__/graphics/icons/radar.png"
 dart_radar_item.icon_size = 64
 dart_radar_item.icon_mipmaps = 4
 dart_radar_item.order = order .. "-a"
@@ -169,7 +172,7 @@ Log.logBlock(dart_fcc_item, function(m)log(m)end, Log.FINER)
 local dart_tech = {
     name = 'dart-radar',
     type = 'technology',
-    icon = "__base__/graphics/technology/radar.png", -- TODO
+    icon = "__dart__/graphics/technology/radar.png",
     icon_size = 256,
     icon_mipmaps = 4,
 
@@ -191,23 +194,6 @@ local dart_tech = {
 
     order = "c-e-b2",
 }
--- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
---- animation for the invisible dart_radar (as this is a constant combinator, it doesn't support animation itself
---- so this must be done in control (in the on_build_entity / on_space_platform_built_entity event)
-local dart_radar_animation = {
-    type = "animation",
-    name = "dart-radar-animation",
-    stripes = {
-        { filename = "__base__/graphics/entity/radar/radar.png", width_in_frames = 8, height_in_frames = 8 },
-    },
-    animation_speed = 1.4,
-    frame_count = 64,
-    width = 196,
-    height = 254,
-    scale = 1 / 6,
-    run_mode = "backward",
-}
 
 -- make all usable
 data:extend({
@@ -218,5 +204,4 @@ data:extend({
     dart_radar_recipe,
     dart_fcc_recipe,
     dart_tech,
-    dart_radar_animation,
 })
