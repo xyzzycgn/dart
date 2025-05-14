69c46863face94ec597b327e8eab32ac92cdacd3
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-05-14 14:43:52 +0200

    sorted entries

--- scripts/gui/styles.lua
+++ scripts/gui/styles.lua
@@ -1,4 +1,3 @@
----
 --- Created by xyzzycgn.
 --- DateTime: 26.12.24 06:01
 ---
@@ -7,14 +6,27 @@ local flib_data_util = require("__flib__.data-util")
 
 local styles = data.raw["gui-style"].default
 
-styles.dart_top_frame = {
-    type = "frame_style",
-    vertically_stretchable = "on",
-    horizontally_stretchable = "on",
-    vertically_squashable = "on",
-    horizontally_squashable = "on",
-    maximal_height = 600,
-    maximal_width = 1000,
+local function graphical_set(filename)
+    return { filename = "__core__/graphics/arrows/" .. filename, size = { 16, 16 }, scale = 0.5, }
+end
+
+local empty_checkmark = {
+    filename = flib_data_util.empty_image,
+    priority = "very-low",
+    width = 1,
+    height = 1,
+    frame_count = 1,
+    scale = 8,
+}
+
+styles.dart_camera = {
+    type = "camera_style",
+    size = 90,
+}
+
+styles.dart_camera_wide = {
+    type = "camera_style",
+    size = 180,
 }
 
 styles.dart_content_frame = {
@@ -22,15 +34,6 @@ styles.dart_content_frame = {
     parent = "inside_shallow_frame_with_padding",
     vertically_stretchable = "off",
     horizontally_stretchable = "on",
-}
-
-styles.dart_draggable_space_header = {
-    type = "empty_widget_style",
-    parent = "draggable_space_header",
-    horizontally_stretchable = "on",
-    left_margin = 4,
-    right_margin = 4,
-    height = 24,
 }
 
 styles.dart_controls_flow = {
@@ -42,12 +45,6 @@ styles.dart_controls_flow = {
 styles.dart_controls_textfield = {
     type = "textbox_style",
     width = 36
-}
-
-styles.dart_stretchable_label_style = {
-    parent = "label",
-    horizontally_stretchable = "stretch_and_expand",
-    type = "label_style"
 }
 
 styles.dart_deep_frame = {
@@ -63,15 +60,27 @@ styles.dart_deep_frame = {
     bottom_margin = 4
 }
 
-styles.dart_tabbed_pane = {
-    type = "tabbed_pane_style",
-    tab_content_frame = {
-        type = "frame_style",
-        parent = "tabbed_pane_frame",
-        left_padding = 12,
-        right_padding = 12,
-        bottom_padding = 8,
-    },
+styles.dart_draggable_space_header = {
+    type = "empty_widget_style",
+    parent = "draggable_space_header",
+    horizontally_stretchable = "on",
+    left_margin = 4,
+    right_margin = 4,
+    height = 24,
+}
+
+styles.dart_minimap = {
+    type = "minimap_style",
+    size = 90,
+}
+
+styles.dart_minimap_label = {
+    type = "label_style",
+    font = "default-small",
+    font_color = { 0, 0.7, 0 },
+    width = 90,
+    vertical_align = "bottom",
+    horizontal_align = "left",
 }
 
 styles.dart_scrollpane_style = {
@@ -85,20 +94,12 @@ styles.dart_scrollpane_style = {
     vertically_stretchable = "on",
 }
 
-styles.dart_table_style = {
-    type = "table_style",
-    horizontally_stretchable = "stretch_and_expand",
-    vertically_stretchable = "on",
-    top_margin = 8,
-    left_margin = 8,
-    right_margin = 8,
-    bottom_margin = 4
-}
-
-styles.dart_small_slot_table_frame = {
-  type = "frame_style",
-  minimal_height = 36,
-  padding = 0,
+-- selected is orange by default
+styles.dart_selected_sort_checkbox = {
+    type = "checkbox_style",
+    parent = "dart_sort_checkbox",
+    default_graphical_set = graphical_set("table-header-sort-arrow-down-active.png"),
+    selected_graphical_set = graphical_set("table-header-sort-arrow-up-active.png"),
 }
 
 for _, color in ipairs({ "default", "red", "green", "blue" }) do
@@ -109,43 +110,10 @@ for _, color in ipairs({ "default", "red", "green", "blue" }) do
     }
 end
 
-
-
-styles.dart_minimap = {
-  type = "minimap_style",
-  size = 90,
-}
-
-styles.dart_minimap_label = {
-  type = "label_style",
-  font = "default-small",
-  font_color = { 0, 0.7, 0 },
-  width = 90,
-  vertical_align = "bottom",
-  horizontal_align = "left",
-}
-
-styles.dart_camera = {
-    type = "camera_style",
-    size = 90,
-}
-
-styles.dart_camera_wide = {
-    type = "camera_style",
-    size = 180,
-}
-
-local function graphical_set(filename)
-    return { filename = "__core__/graphics/arrows/" .. filename, size = { 16, 16 }, scale = 0.5, }
-end
-
-local empty_checkmark = {
-    filename = flib_data_util.empty_image,
-    priority = "very-low",
-    width = 1,
-    height = 1,
-    frame_count = 1,
-    scale = 8,
+styles.dart_small_slot_table_frame = {
+    type = "frame_style",
+    minimal_height = 36,
+    padding = 0,
 }
 
 -- inactive is grey until hovered
@@ -168,10 +136,39 @@ styles.dart_sort_checkbox = {
     horizontally_stretchable = "stretch_and_expand",
 }
 
--- selected is orange by default
-styles.dart_selected_sort_checkbox = {
-    type = "checkbox_style",
-    parent = "dart_sort_checkbox",
-    default_graphical_set = graphical_set("table-header-sort-arrow-down-active.png"),
-    selected_graphical_set = graphical_set("table-header-sort-arrow-up-active.png"),
+styles.dart_stretchable_label_style = {
+    parent = "label",
+    horizontally_stretchable = "stretch_and_expand",
+    type = "label_style"
+}
+
+styles.dart_tabbed_pane = {
+    type = "tabbed_pane_style",
+    tab_content_frame = {
+        type = "frame_style",
+        parent = "tabbed_pane_frame",
+        left_padding = 12,
+        right_padding = 12,
+        bottom_padding = 8,
+    },
+}
+
+styles.dart_table_style = {
+    type = "table_style",
+    horizontally_stretchable = "stretch_and_expand",
+    vertically_stretchable = "on",
+    top_margin = 8,
+    left_margin = 8,
+    right_margin = 8,
+    bottom_margin = 4
+}
+
+styles.dart_top_frame = {
+    type = "frame_style",
+    vertically_stretchable = "on",
+    horizontally_stretchable = "on",
+    vertically_squashable = "on",
+    horizontally_squashable = "on",
+    maximal_height = 600,
+    maximal_width = 1000,
 }
