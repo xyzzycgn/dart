3056f701ec2b3285b073c9cf241241f0c6dd5045
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-07-30 21:43:29 +0200

    refactoring

--- data-updates.lua
+++ data-updates.lua
@@ -14,75 +14,69 @@ local function addCircuitConnector(turret_name, ccd_name)
     turret.circuit_connector = circuit_connector_definitions[ccd_name]
     turret.circuit_wire_max_distance = default_circuit_wire_max_distance
 end
+
+local function createVariationsAndAddCircuitConnector(ccd_name, listOfVariations, turret_name)
+    circuit_connector_definitions[ccd_name] = circuit_connector_definitions.create_vector(universal_connector_template, listOfVariations)
+    addCircuitConnector(turret_name, ccd_name)
+end
 -- ###############################################################
 
 if mods['vtk-cannon-turret'] then
     Log.log("mod vtk-cannon-turret detected", function(m)log(m)end, Log.CONFIG)
 
-    circuit_connector_definitions["dart-turret-vtk"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+    createVariationsAndAddCircuitConnector("dart-turret-vtk", {
         createVariation(17, -20, 15),
         createVariation(17, -20, 15),
         createVariation(17, -20, 15),
         createVariation(17, -20, 15),
-    })
+    }, "vtk-cannon-turret")
 
-    circuit_connector_definitions["dart-turret-heavy-vtk"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+    createVariationsAndAddCircuitConnector("dart-turret-heavy-vtk", {
         createVariation(17,   -31,  21),
         createVariation(17,   -31,  21),
         createVariation(17,   -31,  21),
         createVariation(17,   -31,  21),
-    })
-
-    addCircuitConnector("vtk-cannon-turret", "dart-turret-vtk")
-    addCircuitConnector("vtk-cannon-turret-heavy", "dart-turret-heavy-vtk")
+    }, "vtk-cannon-turret-heavy")
 end
 -- ###############################################################
 
 if mods['RampantArsenalFork'] then
     Log.log("mod RampantArsenalFork detected", function(m)log(m)end, Log.CONFIG)
 
-    circuit_connector_definitions["dart-cannon-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
-        createVariation(17,   -41.5,  24),
-        createVariation(17,   -41.5,  24),
-        createVariation(17,   -41.5,  24),
-        createVariation(17,   -41.5,  24),
-    })
-
-    circuit_connector_definitions["dart-rapid-cannon-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
-        createVariation(26,   0,  28),
-        createVariation(26,   0,  28),
-        createVariation(26,   0,  28),
-        createVariation(26,   0,  28),
-    })
-
-    circuit_connector_definitions["dart-rocket-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
-        createVariation(31, 21, 24),
-        createVariation(31, 21, 24),
-        createVariation(31, 21, 24),
-        createVariation(31, 21, 24),
-    })
-
-    circuit_connector_definitions["dart-rapid-rocket-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
-        createVariation(0, 6, 24),
-        createVariation(0, 6, 24),
-        createVariation(0, 6, 24),
-        createVariation(0, 6, 24),
-    })
-
-    circuit_connector_definitions["dart-gun-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
-        createVariation(33, 15, 21),
-    })
-
     local function rampant_name(name)
         return name .. "-ammo-turret-rampant-arsenal"
     end
 
-    addCircuitConnector(rampant_name("cannon"), "dart-cannon-rampant")
-    addCircuitConnector(rampant_name("rapid-cannon"), "dart-rapid-cannon-rampant")
+    createVariationsAndAddCircuitConnector("dart-cannon-rampant", {
+        createVariation(17,   -41.5,  24),
+        createVariation(17,   -41.5,  24),
+        createVariation(17,   -41.5,  24),
+        createVariation(17,   -41.5,  24),
+    }, rampant_name("cannon"))
 
-    addCircuitConnector(rampant_name("rocket"), "dart-rocket-rampant")
-    addCircuitConnector(rampant_name("rapid-rocket"), "dart-rapid-rocket-rampant")
+    createVariationsAndAddCircuitConnector("dart-rapid-cannon-rampant", {
+        createVariation(26,   0,  28),
+        createVariation(26,   0,  28),
+        createVariation(26,   0,  28),
+        createVariation(26,   0,  28),
+    }, rampant_name("rapid-cannon"))
 
-    addCircuitConnector(rampant_name("gun"), "dart-gun-rampant")
+    createVariationsAndAddCircuitConnector("dart-rocket-rampant", {
+        createVariation(31, 21, 24),
+        createVariation(31, 21, 24),
+        createVariation(31, 21, 24),
+        createVariation(31, 21, 24),
+    }, rampant_name("rocket"))
+
+    createVariationsAndAddCircuitConnector("dart-rapid-rocket-rampant", {
+        createVariation(0, 6, 24),
+        createVariation(0, 6, 24),
+        createVariation(0, 6, 24),
+        createVariation(0, 6, 24),
+    }, rampant_name("rapid-rocket"))
+
+    createVariationsAndAddCircuitConnector("dart-gun-rampant", {
+        createVariation(33, 15, 21),
+    }, rampant_name("gun"))
 end
 
