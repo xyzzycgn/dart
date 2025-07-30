426e0dd01e6599e124a0cb796628b87c16390e47
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-07-30 18:31:36 +0200

    add missing circuit-connector to some turrets from mod RampantArsenalFork (those making sense as part of  asteroid defense)

--- data-updates.lua
+++ data-updates.lua
@@ -5,9 +5,16 @@
 local Log = require("__log4factorio__.Log")
 Log.setSeverity(Log.CONFIG)
 
-local function createVariation(num, x, y)
-    return { variation = num, main_offset = util.by_pixel(x, y), shadow_offset = util.by_pixel(0, 0), show_shadow = false }
+local function createVariation(variation, x, y)
+    return { variation = variation, main_offset = util.by_pixel(x, y), shadow_offset = util.by_pixel(0, 0), show_shadow = false }
 end
+
+local function addCircuitConnector(turret_name, ccd_name)
+    local turret = data.raw["ammo-turret"][turret_name]
+    turret.circuit_connector = circuit_connector_definitions[ccd_name]
+    turret.circuit_wire_max_distance = default_circuit_wire_max_distance
+end
+-- ###############################################################
 
 if mods['vtk-cannon-turret'] then
     Log.log("mod vtk-cannon-turret detected", function(m)log(m)end, Log.CONFIG)
@@ -26,13 +33,56 @@ if mods['vtk-cannon-turret'] then
         createVariation(17,   -31,  21),
     })
 
-    local turret = data.raw["ammo-turret"]["vtk-cannon-turret"]
-    turret.circuit_connector = circuit_connector_definitions["dart-turret-vtk"]
-    turret.circuit_wire_max_distance = default_circuit_wire_max_distance
-
-    local hturret = data.raw["ammo-turret"]["vtk-cannon-turret-heavy"]
-    hturret.circuit_connector = circuit_connector_definitions["dart-turret-heavy-vtk"]
-    hturret.circuit_wire_max_distance = default_circuit_wire_max_distance
+    addCircuitConnector("vtk-cannon-turret", "dart-turret-vtk")
+    addCircuitConnector("vtk-cannon-turret-heavy", "dart-turret-heavy-vtk")
 end
+-- ###############################################################
 
+if mods['RampantArsenalFork'] then
+    Log.log("mod RampantArsenalFork detected", function(m)log(m)end, Log.CONFIG)
+
+    circuit_connector_definitions["dart-cannon-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+        createVariation(17,   -41.5,  24),
+        createVariation(17,   -41.5,  24),
+        createVariation(17,   -41.5,  24),
+        createVariation(17,   -41.5,  24),
+    })
+
+    circuit_connector_definitions["dart-rapid-cannon-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+        createVariation(26,   0,  28),
+        createVariation(26,   0,  28),
+        createVariation(26,   0,  28),
+        createVariation(26,   0,  28),
+    })
+
+    circuit_connector_definitions["dart-rocket-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+        createVariation(31, 21, 24),
+        createVariation(31, 21, 24),
+        createVariation(31, 21, 24),
+        createVariation(31, 21, 24),
+    })
+
+    circuit_connector_definitions["dart-rapid-rocket-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+        createVariation(0, 6, 24),
+        createVariation(0, 6, 24),
+        createVariation(0, 6, 24),
+        createVariation(0, 6, 24),
+    })
+
+    circuit_connector_definitions["dart-gun-rampant"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+        createVariation(33, 15, 21),
+    })
+
+    local function rampant_name(name)
+        return name .. "-ammo-turret-rampant-arsenal"
+    end
+
+    addCircuitConnector(rampant_name("cannon"), "dart-cannon-rampant")
+    addCircuitConnector(rampant_name("rapid-cannon"), "dart-rapid-cannon-rampant")
+
+    addCircuitConnector(rampant_name("rocket"), "dart-rocket-rampant")
+    addCircuitConnector(rampant_name("rapid-rocket"), "dart-rapid-rocket-rampant")
+
+    addCircuitConnector(rampant_name("gun"), "dart-gun-rampant")
+end
 
