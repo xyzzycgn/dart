b7c4e4a74af5f7f5a6d9daaf7607925207a88adf
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-07-30 13:37:00 +0200

    added missing circuit-connector to turrets from mod vtk-cannon-turret

--- data-updates.lua
+++ data-updates.lua
@@ -9,43 +9,30 @@ local function createVariation(num, x, y)
     return { variation = num, main_offset = util.by_pixel(x, y), shadow_offset = util.by_pixel(0, 0), show_shadow = false }
 end
 
-Log.logBlock(circuit_connector_definitions, function(m)log(m)end, Log.CONFIG)
-
-circuit_connector_definitions["dart-turret"] = circuit_connector_definitions.create_vector(universal_connector_template, {
-    --createVariation(0,   6,  24),
-    --createVariation(7, -20,  20),
-    --createVariation(6, -34,   0),
-    --createVariation(5, -37,  -8),
-    --createVariation(4, -37,  -8),
-    --createVariation(3,  34, -12),
-    --createVariation(2,  37,  -8),
-    --createVariation(1,  30,  16)
-
-    createVariation(0,   0,  20),
-    createVariation(6, -20,   0),
-    createVariation(4,   0, -20),
-    createVariation(2,  20,   0),
-})
-
-Log.logBlock(circuit_connector_definitions["dart-turret"], function(m)log(m)end, Log.CONFIG)
-
 if mods['vtk-cannon-turret'] then
     Log.log("mod vtk-cannon-turret detected", function(m)log(m)end, Log.CONFIG)
 
-  --table.insert(data.raw["ammo-turret"]["vtk-cannon-turret"], {
-  --   circuit_connector = circuit_connector_definitions["dart-turret"],
-  --   circuit_wire_max_distance = default_circuit_wire_max_distance,
-  --})
+    circuit_connector_definitions["dart-turret-vtk"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+        createVariation(17, -20, 15),
+        createVariation(17, -20, 15),
+        createVariation(17, -20, 15),
+        createVariation(17, -20, 15),
+    })
+
+    circuit_connector_definitions["dart-turret-heavy-vtk"] = circuit_connector_definitions.create_vector(universal_connector_template, {
+        createVariation(17,   -31,  21),
+        createVariation(17,   -31,  21),
+        createVariation(17,   -31,  21),
+        createVariation(17,   -31,  21),
+    })
+
     local turret = data.raw["ammo-turret"]["vtk-cannon-turret"]
-    turret.circuit_connector = circuit_connector_definitions["dart-turret"]
+    turret.circuit_connector = circuit_connector_definitions["dart-turret-vtk"]
     turret.circuit_wire_max_distance = default_circuit_wire_max_distance
 
     local hturret = data.raw["ammo-turret"]["vtk-cannon-turret-heavy"]
-    hturret.circuit_connector = circuit_connector_definitions["dart-turret"]
+    hturret.circuit_connector = circuit_connector_definitions["dart-turret-heavy-vtk"]
     hturret.circuit_wire_max_distance = default_circuit_wire_max_distance
-
-    Log.logBlock(data.raw["ammo-turret"]["vtk-cannon-turret"], function(m)log(m)end, Log.CONFIG)
-    Log.logBlock(data.raw["ammo-turret"]["vtk-cannon-turret-heavy"], function(m)log(m)end, Log.CONFIG)
 end
 
 
