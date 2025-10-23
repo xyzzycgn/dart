e3f84fd97f060d1720f1bef68cfaf74ce6ce0b7c
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-10-23 10:30:25 +0200

    more details in log message

--- migrations/1.1.0.lua
+++ migrations/1.1.0.lua
@@ -16,19 +16,26 @@ end
 
 -- register all fccs, radars and ammo-turrets (for usage in "remove all entities" in editor mode - see ticket #52)
 local registeredEntities = global_data.getRegisteredEntities()
+local nfccs, nradars, nturrets, nplatforms = 0, 0, 0, 0
 for _, pons in pairs(global_data.getPlatforms()) do
     for _, fop in pairs(pons.fccsOnPlatform) do
         register(registeredEntities, fop.fcc, pons.fccsOnPlatform)
+        nfccs = nfccs + 1
     end
 
     for _, rop in pairs(pons.radarsOnPlatform) do
         register(registeredEntities, rop.radar, pons.radarsOnPlatform)
+        nradars = nradars + 1
     end
 
     for _, top in pairs(pons.turretsOnPlatform) do
         register(registeredEntities, top.turret, pons.turretsOnPlatform)
+        nturrets = nturrets + 1
     end
+
+    nplatforms = nplatforms + 1
 end
 
-log(serpent.block(registeredEntities))
+log(string.format("registered %d FCCs, %d radars and %d turrets on %d platforms for on_object_destroyed",
+        nfccs, nradars, nturrets, nplatforms))
 log("migration to 1.1.0 finished")
